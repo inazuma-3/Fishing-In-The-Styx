46ffb61e0d5f572e7b46badb5111815803eb0a04
00kae <nguyenmisha00@gmail.com>
2026-03-20 22:48:48 -0700

    feat: added bobber BUT BOY SHES FLYING

--- Main/scripts/player.gd
+++ Main/scripts/player.gd
@@ -4,6 +4,11 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+func _input(event):
+	var boob = preload("res://Main/scenes/System/Player/bobber.tscn").instantiate()
+	if event is InputEventMouseButton:
+		print("Mouse Click/Unclick at: ", event.position)
+		add_child(boob)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
